337f92d5071c43a57bac4a7c232df69374225bf4
Harsh2003-oss <harsh.sil872@gmail.com>
2025-08-03 01:00:52 +0530

    added cors

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+const cors = require('cors')
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
